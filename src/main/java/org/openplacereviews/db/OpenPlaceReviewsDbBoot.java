fd00178070e2064f61c89b86c6d410d0ed6edf26
Victor Shcherb <victor.shcherb@gmail.com>
2019-05-20 20:42:21 +0200

    Fix package component scan

--- src/main/java/org/openplacereviews/db/OpenPlaceReviewsDbBoot.java
+++ src/main/java/org/openplacereviews/db/OpenPlaceReviewsDbBoot.java
@@ -4,8 +4,10 @@ package org.openplacereviews.db;
 import org.openplacereviews.opendb.OpenDBServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.openplacereviews")
 public class OpenPlaceReviewsDbBoot extends OpenDBServer {
 
 	public static void main(String[] args) {
