d00f67fa2a3256ed5982ed1ecfead10d9c874d68
Mykhailo Savchuk <msavchuk@vareger.com>
2019-06-14 15:59:39 +0300

    User pre start method instead configuration

--- openplacereviews-boot/src/main/java/org/openplacereviews/db/OpenPlaceReviewsDbBoot.java
+++ openplacereviews-boot/src/main/java/org/openplacereviews/db/OpenPlaceReviewsDbBoot.java
@@ -2,6 +2,7 @@ package org.openplacereviews.db;
 
 
 import org.openplacereviews.opendb.OpenDBServer;
+import org.openplacereviews.opendb.service.BlocksManager;
 import org.openplacereviews.opendb.service.DBSchemaManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -12,9 +13,17 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("org.openplacereviews")
 public class OpenPlaceReviewsDbBoot extends OpenDBServer {
 
+	@Autowired
+	public BlocksManager blocksManager;
+
 	public static void main(String[] args) {
 		System.setProperty("spring.devtools.restart.enabled", "false");
 		SpringApplication.run(OpenPlaceReviewsDbBoot.class, args);
+	}
+
+	@Override
+	public void preStartApplication() {
+		blocksManager.addToBootstrap("opr-osm");
 	}
 
 }
