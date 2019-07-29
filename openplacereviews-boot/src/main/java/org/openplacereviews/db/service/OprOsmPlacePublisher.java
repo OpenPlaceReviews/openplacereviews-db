71661e8b17cc31414a8f8b89dfdcd88779a5f7d6
Eduard Vavrynchuk <edikvavrinchuk@gmail.com>
2019-07-29 17:42:16 +0300

    added diff operation

--- openplacereviews-boot/src/main/java/org/openplacereviews/db/service/OprOsmPlacePublisher.java
+++ openplacereviews-boot/src/main/java/org/openplacereviews/db/service/OprOsmPlacePublisher.java
@@ -8,12 +8,14 @@ import org.openplacereviews.opendb.service.BlocksManager;
 import org.openplacereviews.opendb.util.exception.FailedVerificationException;
 import org.openplacereviews.osm.OsmLocationTool;
 import org.openplacereviews.osm.OsmParser;
+import org.openplacereviews.osm.model.DiffEntity;
 import org.openplacereviews.osm.model.Entity;
 import org.openplacereviews.osm.model.EntityInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -26,10 +28,10 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static org.openplacereviews.opendb.ops.OpBlockchainRules.OP_OPERATION;
 import static org.openplacereviews.opendb.ops.OpObject.F_ID;
+import static org.openplacereviews.opendb.ops.OpOperation.F_DELETE;
 import static org.openplacereviews.opendb.ops.OpOperation.F_TYPE;
 import static org.openplacereviews.osm.model.Entity.ATTR_LATITUDE;
 import static org.openplacereviews.osm.model.Entity.ATTR_LONGITUDE;
@@ -48,7 +50,7 @@ public class OprOsmPlacePublisher implements ApplicationListener<ApplicationRead
 	public BlocksManager blocksManager;
 
 	@Autowired
-	private DbOprOsmSchemaManager dbSchemaManager;
+	public JdbcTemplate jdbcTemplate;
 
 	protected static final Log LOGGER = LogFactory.getLog(OprOsmPlacePublisher.class);
 
@@ -60,6 +62,9 @@ public class OprOsmPlacePublisher implements ApplicationListener<ApplicationRead
 
 	@Value("${osm.parser.overpass.url}")
 	protected String overpassURL;
+
+	@Value("${osm.parser.overpass.timestamp}")
+	protected String overpassTimestampURL;
 
 	@Value("${osm.parser.operation.type}")
 	private String osmOpType;
@@ -86,15 +91,13 @@ public class OprOsmPlacePublisher implements ApplicationListener<ApplicationRead
 		}
 		if (runImport.equals("true")) {
 			publish();
-			while (dbSchemaManager.countUndeployedPlaces() > 0) {
-				publish();
-			}
 		}
 	}
 
 	protected void publish() {
-		if (!checkSyncStateActive(timestamp)) {
-			OsmParser osmParser = null;
+		String statusSync = getSyncStatus(timestamp);
+		if (statusSync.equals("diff") || statusSync.equals("new")) {
+			OsmParser osmParser;
 			Reader reader;
 			try {
 				reader = retrieveFile(timestamp);
@@ -104,7 +107,7 @@ public class OprOsmPlacePublisher implements ApplicationListener<ApplicationRead
 				throw new IllegalArgumentException("Error while creating osm parser");
 			}
 
-			publish(osmParser);
+			publish(osmParser, statusSync);
 			try {
 				blocksManager.addOperation(generateEditOpForOpOprCrawler(timestamp));
 			} catch (FailedVerificationException e) {
@@ -121,43 +124,41 @@ public class OprOsmPlacePublisher implements ApplicationListener<ApplicationRead
 		}
 	}
 
-	protected void publish(OsmParser osmParser) {
+	protected void publish(OsmParser osmParser, String statusSync) {
 		appStartedMs = System.currentTimeMillis();
+		//List<String> osmPlaceHeaders = blocksManager.getBlockchain().getObjects();
 		while (true) {
 			try {
-				List<Entity> places = osmParser.parseNextCoordinatePlaces(placesPerOperation);
+				Object places;
+				if (statusSync.equals("new")) {
+					places = osmParser.parseNextCoordinatePlaces(placesPerOperation);
+				} else {
+					places = osmParser.parseNextDiffCoordinatePlace(placesPerOperation);
+				}
 
-				if (places == null || places.isEmpty())
+				if (places == null || ((List)places).isEmpty())
 					break;
 
-				if (dbSchemaManager.areSomeExistInDb(places)) {
-					//Has place already deployed.
-					places = places.stream()
-							.filter(place -> !dbSchemaManager.isDeployed(place))
-							.collect(Collectors.toList());
-				}
+				// todo filter duplicates for create op!!!
 				publish(places);
 			} catch (Exception e) {
-				e.printStackTrace();
+				LOGGER.error("Error while publishing");
 				break;
 			}
 		}
 		LOGGER.info("Amount of operation transferred data: " + opCounter);
 	}
 
-	protected void publish(List<Entity> places) throws FailedVerificationException {
-		if (places.isEmpty())
-			return;
-
-		OpOperation osmCoordinatePlacesDto = generateOpOperationFromPlaceList(places);
+	protected void publish(Object places) throws FailedVerificationException {
+		List<OpOperation> osmCoordinatePlacesDto = generateOpOperationFromPlaceList((List<Object>) places);
 		try {
-			blocksManager.addOperation(osmCoordinatePlacesDto);
+			for (OpOperation opOperation : osmCoordinatePlacesDto) {
+				blocksManager.addOperation(opOperation);
+			}
 			opCounter += 1;
-			dbSchemaManager.insertPlaces(places, true);
 		} catch (Exception e) {
 			LOGGER.error("Error occurred while posting places: " + e.getMessage());
 			LOGGER.info("Amount of pack transferred data: " + opCounter);
-			dbSchemaManager.insertPlaces(places, false);
 		}
 
 		try {
@@ -176,32 +177,143 @@ public class OprOsmPlacePublisher implements ApplicationListener<ApplicationRead
 
 	}
 
-	protected OpOperation generateOpOperationFromPlaceList(List<Entity> places) throws FailedVerificationException {
-		OpOperation opOperation = new OpOperation();
-		opOperation.setType(osmOpType);
-		opOperation.setSignedBy(blocksManager.getServerUser());
+	protected List<OpOperation> generateOpOperationFromPlaceList(List<Object> places) throws FailedVerificationException {
+		List<OpOperation> opOperations = new ArrayList<>();
 
-		for (Entity e : places) {
-			OpObject create = new OpObject();
-			create.setId(OsmLocationTool.generateStrId(e.getLatLon()));
-			TreeMap<String, Object> osmObject = new TreeMap<>();
-			osmObject.put(F_ID, String.valueOf(e.getId()));
-			osmObject.put(F_TYPE, e.getClass().getSimpleName().toLowerCase());
-			osmObject.put(ATTR_TAGS, e.getTags());
-			osmObject.put(ATTR_LONGITUDE, e.getLongitude());
-			osmObject.put(ATTR_LATITUDE, e.getLatitude());
+		OpOperation createOperation = null;
+		OpOperation deleteOperation = null;
+		OpOperation editOperation = null;
 
-			if (e.getEntityInfo() != null) {
-				generateEntityInfo(osmObject, e.getEntityInfo());
+		for (Object e : places) {
+			if (e instanceof Entity) {
+
+				Entity obj = (Entity) e;
+				OpObject create = new OpObject();
+				create.setId(OsmLocationTool.generateStrId(obj.getLatLon(), obj.getId()));
+				TreeMap<String, Object> osmObject = new TreeMap<>();
+				osmObject.put(F_ID, String.valueOf(obj.getId()));
+				osmObject.put(F_TYPE, obj.getClass().getSimpleName().toLowerCase());
+				osmObject.put(ATTR_TAGS, obj.getTags());
+				osmObject.put(ATTR_LONGITUDE, obj.getLongitude());
+				osmObject.put(ATTR_LATITUDE, obj.getLatitude());
+
+				if (obj.getEntityInfo() != null) {
+					generateEntityInfo(osmObject, obj.getEntityInfo());
+				}
+
+				create.putObjectValue(ATTR_OSM, osmObject);
+				createOperation = initOpOperation(createOperation);
+				createOperation.addCreated(create);
+			} else if (e instanceof DiffEntity) {
+				DiffEntity diffEntity = (DiffEntity) e;
+				if (diffEntity.getType().equals(DiffEntity.DiffEntityType.DELETE)) {
+					deleteOperation = initOpOperation(deleteOperation);
+					List<String> objId = Arrays.asList(OsmLocationTool.generateStrId(diffEntity.getOldNode().getLatLon(), diffEntity.getOldNode().getId()));
+					deleteOperation.putObjectValue(F_DELETE, Arrays.asList(objId));
+				} else if (diffEntity.getType().equals(DiffEntity.DiffEntityType.CREATE)) {
+					OpObject create = new OpObject();
+					if (diffEntity.getNewNode() == null) {
+						System.out.println(diffEntity);
+					}
+					if (diffEntity.getNewNode().getLatLon() == null) {
+						System.out.println(diffEntity);
+					}
+					create.setId(OsmLocationTool.generateStrId(diffEntity.getNewNode().getLatLon(), diffEntity.getNewNode().getId()));
+					TreeMap<String, Object> osmObject = new TreeMap<>();
+					osmObject.put(F_ID, String.valueOf(diffEntity.getNewNode().getId()));
+					osmObject.put(F_TYPE, diffEntity.getNewNode().getClass().getSimpleName().toLowerCase());
+					osmObject.put(ATTR_TAGS, diffEntity.getNewNode().getTags());
+					osmObject.put(ATTR_LONGITUDE, diffEntity.getNewNode().getLongitude());
+					osmObject.put(ATTR_LATITUDE, diffEntity.getNewNode().getLatitude());
+
+					if (diffEntity.getNewNode().getEntityInfo() != null) {
+						generateEntityInfo(osmObject, diffEntity.getNewNode().getEntityInfo());
+					}
+
+					create.putObjectValue(ATTR_OSM, osmObject);
+					createOperation = initOpOperation(createOperation);
+					createOperation.addCreated(create);
+				} else if (diffEntity.getType().equals(DiffEntity.DiffEntityType.MODIFY)) {
+					OpObject edit = new OpObject();
+					if (diffEntity.getNewNode().getLatLon().equals(diffEntity.getOldNode().getLatLon())) {
+						edit.setId(OsmLocationTool.generateStrId(diffEntity.getNewNode().getLatLon(), diffEntity.getNewNode().getId()));
+						TreeMap<String, Object> changeTagMap = new TreeMap<>();
+						TreeMap<String, String> currentTagMAp = new TreeMap<>();
+						Map<String, String> tempTags = diffEntity.getOldNode().getTags();
+						for (Map.Entry<String, String> newEntry : diffEntity.getNewNode().getTags().entrySet()) {
+							if (tempTags.containsKey(newEntry.getKey())) {
+								if (!newEntry.getValue().equals(tempTags.get(newEntry.getKey()))) {
+									TreeMap<String, String> setValue = new TreeMap<>();
+									setValue.put("set", newEntry.getValue());
+									changeTagMap.put("osm.tags." + newEntry.getKey(), setValue);
+									currentTagMAp.put("osm.tags." + newEntry.getKey(), tempTags.get(newEntry.getKey()));
+								}
+							} else {
+								TreeMap<String, String> setValue = new TreeMap<>();
+								setValue.put("set", newEntry.getValue());
+								changeTagMap.put("osm.tags." + newEntry.getKey(), setValue);
+							}
+						}
+						tempTags = diffEntity.getNewNode().getTags();
+						for (Map.Entry<String, String> oldEntry : diffEntity.getOldNode().getTags().entrySet()) {
+							if (!tempTags.containsKey(oldEntry.getKey())) {
+								changeTagMap.put("osm.tags." + oldEntry.getKey(), F_DELETE);
+								currentTagMAp.put("osm.tags." + oldEntry.getKey(), oldEntry.getValue());
+							}
+						}
+
+						edit.putObjectValue("change", changeTagMap);
+						edit.putObjectValue("current", currentTagMAp);
+						editOperation = initOpOperation(editOperation);
+						editOperation.addEdited(edit);
+					} else {
+						List<String> objId = Arrays.asList(OsmLocationTool.generateStrId(diffEntity.getNewNode().getLatLon(), diffEntity.getNewNode().getId()));
+						deleteOperation = initOpOperation(deleteOperation);
+						deleteOperation.putObjectValue(F_DELETE,  Arrays.asList(objId));
+						OpObject create = new OpObject();
+						create.setId(OsmLocationTool.generateStrId(diffEntity.getNewNode().getLatLon(), diffEntity.getNewNode().getId()));
+						TreeMap<String, Object> osmObject = new TreeMap<>();
+						osmObject.put(F_ID, String.valueOf(diffEntity.getNewNode().getId()));
+						osmObject.put(F_TYPE, diffEntity.getNewNode().getClass().getSimpleName().toLowerCase());
+						osmObject.put(ATTR_TAGS, diffEntity.getNewNode().getTags());
+						osmObject.put(ATTR_LONGITUDE, diffEntity.getNewNode().getLongitude());
+						osmObject.put(ATTR_LATITUDE, diffEntity.getNewNode().getLatitude());
+
+						if (diffEntity.getNewNode().getEntityInfo() != null) {
+							generateEntityInfo(osmObject, diffEntity.getNewNode().getEntityInfo());
+						}
+						create.putObjectValue(ATTR_OSM, osmObject);
+
+						createOperation = initOpOperation(createOperation);
+						createOperation.addCreated(create);
+					}
+				}
 			}
-
-			create.putObjectValue(ATTR_OSM, osmObject);
-			opOperation.addCreated(create);
 		}
-		String obj = blocksManager.getBlockchain().getRules().getFormatter().opToJson(opOperation);
-		opOperation = blocksManager.getBlockchain().getRules().getFormatter().parseOperation(obj);
 
-		return blocksManager.generateHashAndSign(opOperation, blocksManager.getServerLoginKeyPair());
+		generateHashAndSign(opOperations, deleteOperation);
+		generateHashAndSign(opOperations, createOperation);
+		generateHashAndSign(opOperations, editOperation);
+
+		return opOperations;
+	}
+
+	private OpOperation initOpOperation(OpOperation opOperation) {
+		if (opOperation == null) {
+			opOperation = new OpOperation();
+			opOperation.setType(osmOpType);
+			opOperation.setSignedBy(blocksManager.getServerUser());
+		}
+		return opOperation;
+	}
+
+	private void generateHashAndSign(List<OpOperation> opOperations, OpOperation opOperation) throws FailedVerificationException {
+		if (opOperation != null) {
+			String obj = blocksManager.getBlockchain().getRules().getFormatter().opToJson(opOperation);
+			opOperation = blocksManager.getBlockchain().getRules().getFormatter().parseOperation(obj);
+			opOperation = blocksManager.generateHashAndSign(opOperation, blocksManager.getServerLoginKeyPair());
+			opOperations.add(opOperation);
+		}
 	}
 
 	private void generateEntityInfo(TreeMap<String, Object> osmObject, EntityInfo entityInfo) {
@@ -214,14 +326,19 @@ public class OprOsmPlacePublisher implements ApplicationListener<ApplicationRead
 		osmObject.put(ATTR_ACTION, entityInfo.getAction());
 	}
 
-	private boolean checkSyncStateActive(String timestamp) {
+	// TODO add sync status for DB!!!!!
+	private String getSyncStatus(String timestamp) {
 		Map<String, Object> syncStates = getOpOprCrawler().getListStringObjMap(F_SYNC_STATES).get(0);
 
 		if (syncStates.get(F_DATE).equals(timestamp)) {
-			return true;
+			return "sync";
 		}
 
-		return false;
+		if (syncStates.get(F_DATE).equals("")) {
+			return "new";
+		}
+
+		return "diff";
 	}
 
 	private Reader retrieveFile(String timestamp) throws IOException {
@@ -276,10 +393,23 @@ public class OprOsmPlacePublisher implements ApplicationListener<ApplicationRead
 		changeDate.put(F_SYNC_STATES + "[0]." + F_DATE, setDate);
 		editObject.putObjectValue("change", changeDate);
 		editObject.putObjectValue("current", Collections.EMPTY_MAP);
-		opOperation.putObjectValue("edit", Arrays.asList(editObject));
+		opOperation.addEdited(editObject);
 		blocksManager.generateHashAndSign(opOperation, blocksManager.getServerLoginKeyPair());
 
 		return opOperation;
+	}
+
+	private String getTimestamp() throws IOException {
+		URL url = new URL(overpassTimestampURL);
+
+		//gzip input string
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+		int responseCode = con.getResponseCode();
+		System.out.println("Sending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		return con.getInputStream().toString();
 	}
 
 }
