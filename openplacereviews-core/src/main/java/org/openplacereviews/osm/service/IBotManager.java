7ec5bc82c10c152494463fdff806c1f4b92ab7ae
Victor Shcherb <victor.shcherb@gmail.com>
2019-08-06 17:03:23 +0200

    Add template

--- openplacereviews-core/src/main/java/org/openplacereviews/osm/service/IBotManager.java
+++ openplacereviews-core/src/main/java/org/openplacereviews/osm/service/IBotManager.java
@@ -2,6 +2,6 @@ package org.openplacereviews.osm.service;
 
 import java.util.concurrent.Callable;
 
-public interface IBotManager extends Callable {
+public interface IBotManager<T> extends Callable<T> {
 
 }
