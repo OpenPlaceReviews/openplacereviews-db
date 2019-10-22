243b2e515aa94e9db70228209e5a615461c3bf68
Victor Shcherb <victor.shcherb@gmail.com>
2019-10-22 14:35:15 +0200

    Add public data provider

---
+++ openplacereviews-boot/src/main/java/org/openplacereviews/controllers/OprOSMDataProvider.java
@@ -0,0 +1,124 @@
+package org.openplacereviews.controllers;
+
+import static org.openplacereviews.opendb.ops.OpOperation.F_TYPE;
+import static org.openplacereviews.osm.model.Entity.ATTR_ID;
+import static org.openplacereviews.osm.model.Entity.ATTR_LATITUDE;
+import static org.openplacereviews.osm.model.Entity.ATTR_LONGITUDE;
+import static org.openplacereviews.osm.model.EntityInfo.ATTR_ACTION;
+import static org.openplacereviews.osm.model.EntityInfo.ATTR_CHANGESET;
+import static org.openplacereviews.osm.model.EntityInfo.ATTR_TIMESTAMP;
+import static org.openplacereviews.osm.model.EntityInfo.ATTR_UID;
+import static org.openplacereviews.osm.model.EntityInfo.ATTR_USER;
+import static org.openplacereviews.osm.model.EntityInfo.ATTR_VERSION;
+import static org.openplacereviews.osm.model.EntityInfo.ATTR_VISIBLE;
+import static org.openplacereviews.osm.util.PlaceOpObjectHelper.F_OSM;
+import static org.openplacereviews.osm.util.PlaceOpObjectHelper.F_SOURCE;
+import static org.openplacereviews.osm.util.PlaceOpObjectHelper.F_TAGS;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.openplacereviews.opendb.ops.OpBlockChain;
+import org.openplacereviews.opendb.ops.OpObject;
+import org.openplacereviews.opendb.service.BlocksManager;
+import org.openplacereviews.opendb.service.PublicDataManager.PublicDataProvider;
+import org.openplacereviews.osm.model.Entity;
+import org.openplacereviews.osm.model.EntityInfo;
+import org.openplacereviews.osm.model.Node;
+import org.openplacereviews.osm.model.Relation;
+import org.openplacereviews.osm.model.Way;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.AbstractResource;
+
+public class OprOSMDataProvider implements PublicDataProvider {
+	@Autowired
+	private BlocksManager blocksManager;
+	
+	@Override
+	public AbstractResource getContent() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public AbstractResource getPage() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public Collection<Entity> getOsmObjects() {
+		OpBlockChain blc = blocksManager.getBlockchain();
+		OpBlockChain.ObjectsSearchRequest r = new OpBlockChain.ObjectsSearchRequest();
+		blc.fetchAllObjects("opr.place", r);
+		LinkedList<Entity> osmList = new LinkedList<>();
+		for (OpObject opObject : r.result) {
+			osmList.addAll(generateEntityFromObject(opObject));
+		}
+
+		return osmList;
+	}
+
+	
+
+	private List<Entity> generateEntityFromObject(OpObject opObject) {
+		List<Map<String, Object>> objectEntity = (List<Map<String, Object>>) opObject.getStringObjMap(F_SOURCE).get(F_OSM);
+		List<Entity> entities = new ArrayList<>();
+		for (Map<String, Object> e : objectEntity) {
+			switch ((String)e.get(F_TYPE)) {
+				case "way" : {
+					entities.add(generateWay(e));
+					break;
+				}
+				case "node" : {
+					entities.add(generateNode(e));
+					break;
+				}
+				case "relation" : {
+					entities.add(generateRelation(e));
+					break;
+				}
+				default: {
+					break;
+				}
+			}
+		}
+
+		return entities;
+	}
+
+	private Entity generateRelation(Map<String, Object> map) {
+		Relation relation = new Relation((Long)map.get(ATTR_ID));
+		return relation;
+	}
+
+	private Entity generateNode(Map<String, Object> map) {
+		Node node = new Node((Double) map.get(ATTR_LATITUDE), (Double) map.get(ATTR_LONGITUDE), (Long) map.get(ATTR_ID));
+		for (Map.Entry<String, Object> entry : ((Map<String, Object>) map.get(F_TAGS)).entrySet()) {
+			node.putTag(entry.getKey(), entry.getValue().toString());
+		}
+		generateEntityInfo(map, node);
+		return node;
+	}
+
+	private Entity generateWay(Map<String, Object> map) {
+		Way way = new Way((Long)map.get(ATTR_ID), null, (Double) map.get(ATTR_LATITUDE), (Double) map.get(ATTR_LONGITUDE));
+		for (Map.Entry<String, Object> entry : ((Map<String, Object>) map.get(F_TAGS)).entrySet()) {
+			way.putTag(entry.getKey(), entry.getValue().toString());
+		}
+		generateEntityInfo(map, way);
+		return way;
+	}
+
+	private void generateEntityInfo(Map<String, Object> map, Entity node) {
+		EntityInfo entityInfo = new EntityInfo();
+		entityInfo.setVersion((String) map.get(ATTR_VERSION));
+		entityInfo.setUser((String) map.get(ATTR_USER));
+		entityInfo.setUid((String) map.get(ATTR_UID));
+		entityInfo.setTimestamp((String) map.get(ATTR_TIMESTAMP));
+		entityInfo.setChangeset((String) map.get(ATTR_CHANGESET));
+		entityInfo.setAction((String) map.get(ATTR_ACTION));
+		entityInfo.setVisible((String) map.get(ATTR_VISIBLE));
+		node.setEntityInfo(entityInfo);
+	}
+}
--- openplacereviews-boot/src/main/java/org/openplacereviews/controllers/OprPlaceDataProvider.java
+++ openplacereviews-boot/src/main/java/org/openplacereviews/controllers/OprPlaceDataProvider.java
@@ -1,16 +1,24 @@
 package org.openplacereviews.controllers;
 
+import static org.openplacereviews.osm.model.Entity.ATTR_LATITUDE;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.openplacereviews.opendb.ops.OpBlockChain;
+import org.openplacereviews.opendb.ops.OpIndexColumn;
+import org.openplacereviews.opendb.ops.OpObject;
 import org.openplacereviews.opendb.ops.de.CompoundKey;
 import org.openplacereviews.opendb.service.BlocksManager;
+import org.openplacereviews.opendb.service.DBSchemaManager;
 import org.openplacereviews.opendb.service.PublicDataManager.PublicDataProvider;
 import org.openplacereviews.osm.parser.OsmLocationTool;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.AbstractResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.security.util.InMemoryResource;
 
 import com.github.filosganga.geogson.gson.GeometryAdapterFactory;
 import com.github.filosganga.geogson.model.Feature;
@@ -20,11 +28,14 @@ import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonPrimitive;
 import com.google.openlocationcode.OpenLocationCode;
 
 public class OprPlaceDataProvider implements PublicDataProvider {
 	
+	private static final int TILE_INFO_SUBSET = 4;
+
 	private Gson geoJson;
 	
 	@Autowired
@@ -34,7 +45,38 @@ public class OprPlaceDataProvider implements PublicDataProvider {
 	public OprPlaceDataProvider() {
 		geoJson = new GsonBuilder().registerTypeAdapterFactory(new GeometryAdapterFactory()).create();
 	}
+	
+	public FeatureCollection getIdsByTileId(String tileId) {
+		OpBlockChain blc = blocksManager.getBlockchain();
+		OpBlockChain.ObjectsSearchRequest r = new OpBlockChain.ObjectsSearchRequest();
+		OpIndexColumn ind = blocksManager.getIndex("opr.place", DBSchemaManager.INDEX_P[0]);
+		blc.fetchObjectsByIndex("opr.place", ind, r, tileId);
 
+		return generateFeatureCollectionFromResult(r.result);
+	}
+
+	
+	public FeatureCollection generateFeatureCollectionFromResult(List<OpObject> opObjects) {
+		FeatureCollection fc = new FeatureCollection(new ArrayList<>());
+		for(OpObject o : opObjects) {
+			List<Map<String, Object>> osmList = o.getField(null, "source", "osm");
+			if(osmList.size() == 0) {
+				continue;
+			}
+			Map<String, Object> osm = osmList.get(0);
+			double lat = (double) osm.get(ATTR_LATITUDE);
+			double lon = (double) osm.get(ATTR_LATITUDE);
+			Point p = Point.from(lat, lon);
+			ImmutableMap.Builder<String, JsonElement> bld = ImmutableMap.builder();
+			for(String k : osm.keySet()) {
+				bld.put(k, new JsonPrimitive(osm.get(k).toString()));
+			}
+			Feature f = new Feature(p, bld.build(), Optional.absent());
+			fc.features().add(f);
+		}
+		return fc;
+	}
+	
 	public FeatureCollection getAllIds() {
 		OpBlockChain blc = blocksManager.getBlockchain();
 		OpBlockChain.ObjectsSearchRequest r = new OpBlockChain.ObjectsSearchRequest();
@@ -45,7 +87,7 @@ public class OprPlaceDataProvider implements PublicDataProvider {
 
 		FeatureCollection fc = new FeatureCollection(new ArrayList<>());
 		for(CompoundKey c : ks) {
-			String areaCode = c.first.substring(0, 4);
+			String areaCode = c.first.substring(0, TILE_INFO_SUBSET);
 			Integer l = counts.get(areaCode);
 			if(l == null) {
 				l = 1;
@@ -64,9 +106,15 @@ public class OprPlaceDataProvider implements PublicDataProvider {
 
 		return fc;
 	}
+	
 	@Override
-	public String getContent() {
-		return geoJson.toJson(getAllIds());
+	public AbstractResource getContent() {
+		return new InMemoryResource(geoJson.toJson(getAllIds()));
+	}
+	
+	@Override
+	public AbstractResource getPage() {
+		return new InputStreamResource(OprPlaceDataProvider.class.getResourceAsStream("/map.html"));
 	}
 
 }
