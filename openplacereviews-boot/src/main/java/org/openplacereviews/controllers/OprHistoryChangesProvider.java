04ec28868e96f313fc44fa165863eab7ad23f3fc
Eduard Vavrynchuk <edikvavrinchuk@gmail.com>
2019-11-01 11:30:21 +0200

    added opType (created/deleted/edited) to feature id, also remove entity has been added

--- openplacereviews-boot/src/main/java/org/openplacereviews/controllers/OprHistoryChangesProvider.java
+++ openplacereviews-boot/src/main/java/org/openplacereviews/controllers/OprHistoryChangesProvider.java
@@ -5,7 +5,7 @@ import com.github.filosganga.geogson.model.FeatureCollection;
 import com.github.filosganga.geogson.model.Point;
 import com.google.common.base.Optional;
 import com.google.common.collect.ImmutableMap;
-import com.google.gson.JsonArray;
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
@@ -17,14 +17,22 @@ import org.openplacereviews.opendb.ops.OpBlock;
 import org.openplacereviews.opendb.ops.OpBlockChain;
 import org.openplacereviews.opendb.ops.OpObject;
 import org.openplacereviews.opendb.ops.OpOperation;
+import org.openplacereviews.opendb.service.HistoryManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.AbstractResource;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.security.core.parameters.P;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+import static org.openplacereviews.opendb.ops.OpObject.F_CHANGE;
+import static org.openplacereviews.opendb.ops.OpObject.F_CURRENT;
+import static org.openplacereviews.opendb.ops.OpObject.F_ID;
 import static org.openplacereviews.opendb.ops.OpOperation.F_TYPE;
+import static org.openplacereviews.opendb.service.HistoryManager.DESC_SORT;
+import static org.openplacereviews.opendb.service.HistoryManager.HISTORY_BY_OBJECT;
 import static org.openplacereviews.osm.model.Entity.ATTR_LATITUDE;
 import static org.openplacereviews.osm.model.Entity.ATTR_LONGITUDE;
 import static org.openplacereviews.osm.util.PlaceOpObjectHelper.*;
@@ -33,17 +41,27 @@ public class OprHistoryChangesProvider extends OprPlaceDataProvider {
 
 	private static final Log LOGGER = LogFactory.getLog(ApiController.class);
 
-	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
-	public static final String OPR_PLACE = "opr.place";
 	public static final String PARAM_OSM_DATE = "date";
+
 	public static final String OSM_ID = "osm_id";
 	public static final String OSM_TYPE = "osm_type";
-	public static final String OPR_ID = "opr_id";
 	public static final String OSM_INDEX = "osm_index";
+
 	public static final String BLOCK_TIMESTAMP = "block_timestamp";
 	public static final String BLOCK_HASH = "block_hash";
 	public static final String BLOCK_ID = "block_id";
 	public static final String OP_HASH = "op_hash";
+	public static final String OPR_ID = "opr_id";
+	public static final String OPR_PLACE = "opr.place";
+
+	public static final String OBJ_CREATED = "Created";
+	public static final String OBJ_EDITED = "Edited";
+	public static final String OBJ_REMOVED = "Removed";
+
+	private static final String SOURCE_OSM_REGEX = "source.osm\\[\\d+]";
+
+	@Autowired
+	private HistoryManager historyManager;
 
 	public void getOsmObjectByDate(String stringDate, FeatureCollection featureCollection) throws ParseException {
 		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(stringDate);
@@ -65,8 +83,12 @@ public class OprHistoryChangesProvider extends OprPlaceDataProvider {
 					for (OpObject opObject : opOperation.getCreated()) {
 						generateCreateEntityFromOpObject(opObject, featureCollection, block, opOperation.getRawHash());
 					}
-					// TODO add edited
-					// TODO add removed
+					for (OpObject opObject : opOperation.getEdited()) {
+						generateEditedEntityFromOpObject(opObject, featureCollection);
+					}
+					for (List<String> objId : opOperation.getDeleted()) {
+						generateRemovedEntityFromOpObject(objId, featureCollection);
+					}
 				}
 			}
 		}
@@ -76,46 +98,126 @@ public class OprHistoryChangesProvider extends OprPlaceDataProvider {
 	@SuppressWarnings("unchecked")
 	private void generateCreateEntityFromOpObject(OpObject opObject, FeatureCollection featureCollection, OpBlock opBlock, String opHash) {
 		List<Map<String, Object>> osmList = opObject.getField(null, F_SOURCE, F_OSM);
-		if (osmList.size() == 0) {
-			return;
+		for (Map<String, Object> osm : osmList) {
+			double lat = (double) osm.get(ATTR_LATITUDE);
+			double lon = (double) osm.get(ATTR_LONGITUDE);
+			Point p = Point.from(lon, lat);
+			ImmutableMap.Builder<String, JsonElement> bld = ImmutableMap.builder();
+
+			bld.put(OSM_ID, new JsonPrimitive((Number) osm.get(OpObject.F_ID)));
+			bld.put(OSM_TYPE, new JsonPrimitive((String) osm.get(F_TYPE)));
+
+			Map<String, Object> tagsValue = (Map<String, Object>) osm.get(F_TAGS);
+			generateTagsForEntity(bld, tagsValue);
+
+			bld.put(OPR_ID, new JsonPrimitive(opObject.getId().get(0) + "," + opObject.getId().get(1)));
+			bld.put(OSM_INDEX, new JsonPrimitive(0));
+			bld.put(BLOCK_TIMESTAMP, new JsonPrimitive(opBlock.getDateString()));
+			bld.put(BLOCK_HASH, new JsonPrimitive(opBlock.getRawHash()));
+			bld.put(BLOCK_ID, new JsonPrimitive(opBlock.getBlockId()));
+			bld.put(OP_HASH, new JsonPrimitive(opHash));
+			bld.put(ATTR_LATITUDE, new JsonPrimitive(lat));
+			bld.put(ATTR_LONGITUDE, new JsonPrimitive(lon));
+			bld.put(F_VERSION, new JsonPrimitive((String) osm.get(F_VERSION)));
+			bld.put(F_CHANGESET, new JsonPrimitive((String) osm.get(F_CHANGESET)));
+
+			Feature f = new Feature(p, bld.build(), Optional.absent()).withId(OBJ_CREATED);
+			featureCollection.features().add(f);
 		}
-		Map<String, Object> osm = osmList.get(0);
-		double lat = (double) osm.get(ATTR_LATITUDE);
-		double lon = (double) osm.get(ATTR_LONGITUDE);
-		Point p = Point.from(lon, lat);
-		ImmutableMap.Builder<String, JsonElement> bld = ImmutableMap.builder();
+	}
 
-		bld.put(OSM_ID, new JsonPrimitive((Number) osm.get(OpObject.F_ID)));
-		bld.put(OSM_TYPE, new JsonPrimitive((String) osm.get(F_TYPE)));
+	@SuppressWarnings("unchecked")
+	private void generateRemovedEntityFromOpObject(List<String> objId, FeatureCollection featureCollection) {
+		HistoryManager.HistoryObjectRequest historyObjectRequest = new HistoryManager.HistoryObjectRequest(
+				HISTORY_BY_OBJECT,
+				generateSearchStringKey(objId),
+				1,
+				DESC_SORT
+		);
+		historyManager.retrieveHistory(historyObjectRequest);
+		List<HistoryManager.HistoryEdit> historyEdits = historyObjectRequest.historySearchResult;
+		HistoryManager.HistoryEdit lastVersion = historyEdits.get(0);
+		OpObject opObject = lastVersion.getObjEdit();
+		List<Map<String, Object>> osmList = opObject.getField(null, F_SOURCE, F_OSM);
+		for (Map<String, Object> osm : osmList) {
+			double lat = (double) osm.get(ATTR_LATITUDE);
+			double lon = (double) osm.get(ATTR_LONGITUDE);
+			Point p = Point.from(lon, lat);
+			ImmutableMap.Builder<String, JsonElement> bld = ImmutableMap.builder();
 
-		Map<String, Object> tagsValue = (Map<String, Object>) osm.get(F_TAGS);
+			bld.put(OPR_ID, new JsonPrimitive(opObject.getId().get(0) + "," + opObject.getId().get(1)));
+			for (String k : osm.keySet()) {
+				if (k.equals(F_TAGS)) {
+					continue;
+				}
+				bld.put(k, new JsonPrimitive(osm.get(k).toString()));
+			}
+			Map<String, Object> tagsValue = (Map<String, Object>) osm.get(F_TAGS);
+			generateTagsForEntity(bld, tagsValue);
+
+			Feature f = new Feature(p, bld.build(), Optional.absent()).withId(OBJ_REMOVED);
+			featureCollection.features().add(f);
+		}
+
+	}
+
+	@SuppressWarnings("unchecked")
+	private void generateEditedEntityFromOpObject(OpObject opObject, FeatureCollection featureCollection) {
+		Map<String, Object> change = opObject.getStringObjMap(F_CHANGE);
+		Map<String, Object> current = opObject.getStringObjMap(F_CURRENT);
+
+		if (change.size() == 2 && (((String) change.keySet().toArray()[1]).matches(SOURCE_OSM_REGEX))) {
+			for (String key : current.keySet()) {
+				if (key.matches(SOURCE_OSM_REGEX)) {
+					Map<String, Object> osm = (Map<String, Object>) current.get(key);
+					double lat = (double) osm.get(ATTR_LATITUDE);
+					double lon = (double) osm.get(ATTR_LONGITUDE);
+					Point p = Point.from(lon, lat);
+					ImmutableMap.Builder<String, JsonElement> bld = ImmutableMap.builder();
+
+					bld.put(OPR_ID, new JsonPrimitive(opObject.getId().get(0) + "," + opObject.getId().get(1)));
+					for (String k : osm.keySet()) {
+						if (k.equals(F_TAGS)) {
+							continue;
+						}
+						if (k.equals(F_ID)) {
+							bld.put(OSM_ID, new JsonPrimitive((Number) osm.get(k)));
+							continue;
+						}
+						if (k.equals(F_TYPE)) {
+							bld.put(OSM_TYPE, new JsonPrimitive(osm.get(k).toString()));
+							continue;
+						}
+						bld.put(k, new JsonPrimitive(osm.get(k).toString()));
+					}
+					Map<String, Object> tagsValue = (Map<String, Object>) osm.get(F_TAGS);
+					generateTagsForEntity(bld, tagsValue);
+
+					Feature f = new Feature(p, bld.build(), Optional.absent()).withId(OBJ_REMOVED);
+					featureCollection.features().add(f);
+				}
+			}
+		} else {
+			// TODO create edit entity
+		}
+
+	}
+
+	private void generateTagsForEntity(ImmutableMap.Builder<String, JsonElement> bld, Map<String, Object> tagsValue) {
 		if (tagsValue != null) {
 			JsonObject obj = new JsonObject();
-			Iterator<Map.Entry<String, Object>> it = tagsValue.entrySet().iterator();
-			while (it.hasNext()) {
-				Map.Entry<String, Object> e = it.next();
+			for (Map.Entry<String, Object> e : tagsValue.entrySet()) {
 				obj.add(e.getKey(), new JsonPrimitive(e.getValue().toString()));
 			}
 			bld.put(F_TAGS, obj);
 		}
+	}
 
-		JsonArray opr_id = new JsonArray();
-		for (String key: opObject.getId()) {
-			opr_id.add(key);
-		}
-		bld.put(OPR_ID, opr_id);
-		bld.put(OSM_INDEX, new JsonPrimitive(0));
-		bld.put(BLOCK_TIMESTAMP, new JsonPrimitive(opBlock.getDateString()));
-		bld.put(BLOCK_HASH, new JsonPrimitive(opBlock.getRawHash()));
-		bld.put(BLOCK_ID, new JsonPrimitive(opBlock.getBlockId()));
-		bld.put(OP_HASH, new JsonPrimitive(opHash));
-		bld.put(ATTR_LATITUDE, new JsonPrimitive(lat));
-		bld.put(ATTR_LONGITUDE, new JsonPrimitive(lon));
-		bld.put(F_VERSION, new JsonPrimitive((String) osm.get(F_VERSION)));
-		bld.put(F_CHANGESET, new JsonPrimitive((String) osm.get(F_CHANGESET)));
-
-		Feature f = new Feature(p, bld.build(), Optional.absent());
-		featureCollection.features().add(f);
+	private List<String> generateSearchStringKey(List<String> objId) {
+		List<String> searchKey = new ArrayList<>();
+		searchKey.add(OPR_PLACE);
+		searchKey.addAll(objId);
+		return searchKey;
 	}
 
 	@Override
