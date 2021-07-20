777dc98d65cf0262e0c5019ae74015df24132bc0
Victor Shcherb <victor.shcherb@gmail.com>
2021-07-20 19:05:51 +0200

    Fix compilation

--- openplacereviews-core/src/test/java/org/openplacereviews/osm/service/MergePlaceBotOtherTest.java
+++ openplacereviews-core/src/test/java/org/openplacereviews/osm/service/MergePlaceBotOtherTest.java
@@ -78,7 +78,7 @@ public class MergePlaceBotOtherTest {
         bot.TRACE = false;
         
         MergeInfo info = new MergeInfo();
-    	bot.mergePlaces(rs.geo.features(), info);
+    	bot.mergePlaces(rs, info);
     	System.out.println(String.format("Merge places has finished: place groups %d, closed places %d, found similar places %d, merged %d", 
 				info.mergedGroupSize, info.closedPlaces, info.similarPlacesCnt, info.mergedPlacesCnt));
     	Assert.assertTrue(info.mergedPlacesCnt > 2100);
