c3c0d9c6eeda65c385c16ad0f8b26a28cd5d3813
Victor Shcherb <victor.shcherb@gmail.com>
2021-12-11 14:58:17 +0100

    Main patch for all errors

--- openplacereviews-boot/src/main/java/org/openplacereviews/db/OpenPlaceReviewsDbBoot.java
+++ openplacereviews-boot/src/main/java/org/openplacereviews/db/OpenPlaceReviewsDbBoot.java
@@ -105,6 +105,7 @@ public class OpenPlaceReviewsDbBoot extends OpenDBServer implements ApplicationR
 		blocksManager.addPatchOperation(14335, "block_14335");
 		blocksManager.addPatchOperation(14529, "block_14529");
 		blocksManager.addPatchOperation(14586, "block_14586");
+		blocksManager.addPatchOperation(14629, "block_14629");
 		settingsManager.registerTableMapping("obj_opr_places", 2, "opr.place");
 
 		addOsmIdIndex();
